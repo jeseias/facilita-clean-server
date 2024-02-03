6814b7beeaed4c549fe3e428527382bbcbb0bd19
jeseias <domingosjeseias@gmail.com>
2024-02-03 21:23:47 +0100

    feat: init routes

--- src/config/routes.ts
+++ src/config/routes.ts
@@ -1,5 +1,6 @@
-import {FastifyInstance} from 'fastify'
+import { clientRoutes } from "@/modules/clients/http/clients.routes";
+import { FastifyInstance } from "fastify";
 
 export const appRoutes = (app: FastifyInstance) => {
-
-}
+  clientRoutes(app);
+};
--- src/modules/clients/http/clients.routes.ts
+++ src/modules/clients/http/clients.routes.ts
@@ -1,9 +1,10 @@
 import { fastifyRouteAdapter } from "@/adapters/fastify-route-adapter";
 import fastify, { FastifyInstance } from "fastify";
-import { makeRegisterClientController } from "./controllers";
+import { makeLoadClientsController, makeRegisterClientController } from "./controllers";
 
 export const clientRoutes = (app: FastifyInstance) => {
   const ROUTE = "/clients" as const;
 
   app.post(ROUTE, fastifyRouteAdapter(makeRegisterClientController()));
+  app.get(ROUTE, fastifyRouteAdapter(makeLoadClientsController()));
 };
