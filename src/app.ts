1440068e16494256fe36c8ed42060732dc5d904f
jeseias <domingosjeseias@gmail.com>
2024-02-03 21:19:38 +0100

    feat: setup fastify app

---
+++ src/app.ts
@@ -0,0 +1,34 @@
+import cors from "@fastify/cors";
+import fastify from "fastify";
+import { ZodError } from "zod";
+import { env } from "./config/env";
+import { appRoutes } from "./config/routes";
+
+export const app = fastify();
+
+const V1_PREFIX = "/api/v1";
+
+app.register(appRoutes, {
+  prefix: V1_PREFIX,
+});
+
+app.register(cors);
+
+app.get("/", (req, rep) => {
+  return rep.send({ ok: true });
+});
+
+app.setErrorHandler((error, request, reply) => {
+  if (error instanceof ZodError) {
+    return reply.status(400).send({
+      message: "Validation Error",
+      validation: error.format(),
+    });
+  }
+
+  if (env.NODE_ENV !== "prod") {
+    console.error(error);
+  }
+
+  return reply.status(500).send({ message: "internal server error" });
+});
