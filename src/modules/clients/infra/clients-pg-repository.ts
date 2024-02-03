9a0a2bd559528ac38af41df89600ea80c40cf084
jeseias <domingosjeseias@gmail.com>
2024-02-03 21:14:23 +0100

    feat: add CilentPgRepository

---
+++ src/modules/clients/infra/clients-pg-repository.ts
@@ -0,0 +1,26 @@
+import { pgClient } from "@/shared/infra";
+import { ClientRepository } from "../domain/repositories";
+import { CreateClientRepository } from "../domain/repositories/create-client-repository";
+import { LoadClientsRepository } from "../domain/repositories/load-clients-repository";
+
+export class ClientPgRepository implements ClientRepository {
+  async create(
+    params: CreateClientRepository.Params
+  ): CreateClientRepository.Response {
+    const client = await pgClient.query(
+      `INSERT INTO client(name, email, phone) VALUES($1, $2, $3) RETURNING id`,
+      [params.name, params.email, params.phone]
+    );
+
+    console.log(client)
+  }
+
+  async load(
+    params: LoadClientsRepository.Params
+  ): LoadClientsRepository.Response {
+    return {
+      clients: [],
+      totalElements: 0,
+    };
+  }
+}
