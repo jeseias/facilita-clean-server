bfe1a286f4b62dc5ae38796293cbf807df7da956
jeseias <domingosjeseias@gmail.com>
2024-02-03 19:40:56 +0100

    feat: add LoadClientsRepository

--- src/modules/clients/domain/repositories/index.ts
+++ src/modules/clients/domain/repositories/index.ts
@@ -1,3 +1,5 @@
 import { CreateClientRepository } from "./create-client-repository";
+import { LoadClientsRepository } from "./load-clients-repository";
 
-export type ClientRepository = CreateClientRepository.Contract;
+export type ClientRepository = CreateClientRepository.Contract &
+  LoadClientsRepository.Contract;
---
+++ src/modules/clients/domain/repositories/load-clients-repository.ts
@@ -0,0 +1,15 @@
+export namespace LoadClientsRepository {
+  export interface Params {
+    page: number;
+    limit: number;
+  }
+
+  export type Response = Promise<{
+    clients: Client[];
+    totalElements: number;
+  }>;
+
+  export interface Contract {
+    load(params: LoadClientsRepository.Params): LoadClientsRepository.Response;
+  }
+}
