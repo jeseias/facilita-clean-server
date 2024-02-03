adfc6584b3d585f6ac9d43028fe1e0e1b1800f96
jeseias <domingosjeseias@gmail.com>
2024-02-03 21:20:12 +0100

    feat: setup server

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import { app } from "./app";
+import { env } from "./config/env";
+
+app
+  .listen({
+    host: "0.0.0.0",
+    port: env.PORT,
+  })
+  .then(() => console.log("server is running", env.PORT));
